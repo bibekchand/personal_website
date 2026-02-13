bda9907d065c61704488fc25f2da4788e83f6a34
Bibek Chand <bibekchand843@gmail.com>
2026-02-13 08:40:44 +0545

    Add some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import NavBar from "./components/navbar/NavBar.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import TextType from "./components/TextType/TextType.jsx";
 export default function App() {
 	return (
 		<>
 			<NavBar />
-			<div style={{"height":"100vh"}}>Hello, I am Bibek</div>
-			<div>Hello, I am Bibek</div>
+			<div className="h-full flex flex-col justify-center items-center text-3xl gap-5">
+				Hi, I am Bibek 👋
+				<TextType
+					text={[
+						"iOS Development",
+						"Full Stack Web Development",
+						"Android app development",
+					]}
+					typingSpeed={75}
+					pauseDuration={1500}
+					showCursor
+					cursorCharacter="_"
+					texts={[
+						"Welcome to React Bits! Good to see you!",
+						"Build some amazing experiences!",
+					]}
+					deletingSpeed={50}
+					variableSpeedEnabled={false}
+					variableSpeedMin={60}
+					variableSpeedMax={120}
+					cursorBlinkDuration={0.5}
+				/>
+			<button type="button" className="text-2xl border-amber-200 border-2 p-2 hover:bg-amber-200 duration-200 cursor-pointer hover:text-black">
+				Contact Me --&gt;
+			</button>
+			</div>
 		</>
 	);
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,40 @@
 import "./NavBar.css";
-import {useState} from "react"
+import { useState } from "react";
 export default function NavBar() {
-    const [isButtonClicked, toggleButton] = useState(false)
-    const toggleButtonFunction = ()=>{
-        toggleButton(!isButtonClicked)
-    }
+	const [isButtonClicked, toggleButton] = useState(false);
+	const toggleButtonFunction = () => {
+		toggleButton(!isButtonClicked);
+	};
 	return (
-        <>
-        <button type="button" onClick={toggleButtonFunction} className="fixed text-blue-950 z-10 right-0 p-10 cursor-pointer">Menu</button>
-		<nav className="bg-black text-white text-4xl flex justify-center items-center fixed w-full" style={{ height: isButtonClicked ? "100%" : "0"}}>
-			<ul className="cursor-pointer flex flex-col h-full justify-center gap-5 text-center">
-				<div>
-					<li>Home</li>
-					<hr />
-				</div>
-				<div>
-					<li>About</li>
-					<hr />
-				</div>
-				<div>
-					<li>Contact</li>
-					<hr />
-				</div>
-			</ul>
-		</nav>
-        </>
+		<>
+			<button
+				type="button"
+				onClick={toggleButtonFunction}
+				className="fixed text-blue-950 z-10 right-0 p-10 cursor-pointer"
+			>
+				Menu
+			</button>
+			<nav
+				className="text-4xl flex justify-center items-center fixed w-full truncate z-9"
+				style={{
+					height: isButtonClicked ? "100%" : "0",
+				}}
+			>
+				<ul className="cursor-pointer flex flex-col h-full justify-center gap-5 text-center">
+					<div>
+						<li>Home</li>
+						<hr />
+					</div>
+					<div>
+						<li>About</li>
+						<hr />
+					</div>
+					<div>
+						<li>Contact</li>
+						<hr />
+					</div>
+				</ul>
+			</nav>
+		</>
 	);
 }
