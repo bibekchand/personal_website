a4d1a563c580513811c5e11edba2147f7ee227ec
Bibek Chand <bibekchand843@gmail.com>
2026-04-13 17:18:38 +0545

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), react()],
-    base: "/personal_website",
+    base: "/personal_website/",
 });
